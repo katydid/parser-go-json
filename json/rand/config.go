6edc8c9cc395f893eabb864c6b37f239d03b5de4
Walter Schulze <awalterschulze@gmail.com>
2025-02-17 15:06:22 +0000

    more configurable random generation of json values

--- json/rand/config.go
+++ json/rand/config.go
@@ -15,8 +15,14 @@
 package rand
 
 type config struct {
-	// MaxLevels is the maximum number of objects/arrays to generate down. It makes sure the algorithm terminates.
-	maxLevels int
+	// maxDepth is the maximum number of objects/arrays to generate down. It makes sure the algorithm terminates.
+	maxDepth        int
+	maxObjectFields int
+	maxArrayLength  int
+	maxStringLength int
+	numberEdgeCases []string
+	// r.Intn(c.numberEdgeCaseOdds) == 0 will result in a random edge case being generated.
+	numberEdgeCaseOdds int
 }
 
 type Option func(*config)
@@ -24,7 +30,12 @@ type Option func(*config)
 func newConfig(opts ...Option) *config {
 	// default Config
 	c := &config{
-		maxLevels: 5,
+		maxDepth:           5,
+		maxObjectFields:    10,
+		maxArrayLength:     10,
+		maxStringLength:    100,
+		numberEdgeCases:    defaultNumberEdgeCases,
+		numberEdgeCaseOdds: 100,
 	}
 	// apply options
 	for _, o := range opts {
@@ -33,8 +44,38 @@ func newConfig(opts ...Option) *config {
 	return c
 }
 
-func WithMaxLevel(maxLevel int) Option {
+var defaultNumberEdgeCases = []string{
+	"9223372036854775807",                                // math.MaxInt64
+	"9223372036854775808",                                // math.MaxInt64 + 1
+	"-9223372036854775808",                               // math.MinInt64
+	"-9223372036854775809",                               // math.MinInt64 - 1
+	"18446744073709551615",                               // math.MaxUint64
+	"18446744073709551616",                               // math.MaxUint64 + 1
+	"1.79769313486231570814527423731704356798070e+308",   // math.MaxFloat64
+	"2.79769313486231570814527423731704356798070e+308",   // > math.MaxFloat64
+	"4.9406564584124654417656879286822137236505980e-324", // math.SmallestNonzeroFloat64
+}
+
+func WithMaxDepth(maxDepth int) Option {
 	return func(c *config) {
-		c.maxLevels = maxLevel
+		c.maxDepth = maxDepth
+	}
+}
+
+func WithMaxObjectFields(maxObjectFields int) Option {
+	return func(c *config) {
+		c.maxObjectFields = maxObjectFields
+	}
+}
+
+func WithMaxArrayLength(maxArrayLength int) Option {
+	return func(c *config) {
+		c.maxArrayLength = maxArrayLength
+	}
+}
+
+func WithMaxStringLength(maxStringLength int) Option {
+	return func(c *config) {
+		c.maxStringLength = maxStringLength
 	}
 }
